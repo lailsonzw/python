a6b48fb5934efe798edb99ae0af6e305c153b26c
lailsonzw <franciscolailsondealmeida0p9@gmail.com>
2024-01-24 12:36:56 -0300

    novas atividades

---
+++ exercicioscurso/exercicio009.py
@@ -0,0 +1,15 @@
+#META: criar um codigo que imprima a taubada do numero lido;
+
+tamanho = int(input('Digite um numero para ver a sua tabuada ate o 10: '));
+
+print('TABUADA DO {}'.format(tamanho))
+print('{} x {:2} = {}'.format(tamanho,1, tamanho *1))
+print('{} x {:2} = {}'.format(tamanho,2, tamanho *2))
+print('{} x {:2} = {}'.format(tamanho,3, tamanho *3))
+print('{} x {:2} = {}'.format(tamanho,4, tamanho *4))
+print('{} x {:2} = {}'.format(tamanho,5, tamanho *5))
+print('{} x {:2} = {}'.format(tamanho,6, tamanho *6))
+print('{} x {:2} = {}'.format(tamanho,7, tamanho *7))
+print('{} x {:2} = {}'.format(tamanho,8, tamanho *8))
+print('{} x {:2} = {}'.format(tamanho,9, tamanho *9))
+print('{} x {} = {}'.format(tamanho,10, tamanho *10))
---
+++ exercicioscurso/exercicio010.py
@@ -0,0 +1,8 @@
+#META: Criar um codigo que transforme o dinheiro real em dolar;
+
+#quanto custa o dolar?
+#24/01/2024 = 4,92;
+
+real = float(input('Quantos reais voce quer converter em dolar?\nValor do dolar = 4,92\nDigite aqui: '))
+dolar = 4.92
+print("Conversão = {:.4}$".format((real/dolar)))
