3cdb96347871ad4ba2bd86b97c7f3343740b18fa
lailsonzw <franciscolailsondealmeida0p9@gmail.com>
2024-01-27 09:58:33 -0300

    adicionando um projeto pessoal

---
+++ exercicios/coracao.py
@@ -0,0 +1,13 @@
+import turtle
+import tkinter
+
+turtle.color("red")
+turtle.begin_fill()
+turtle.pensize(3)
+turtle.left(50)
+turtle.forward(133)
+turtle.circle(50,200)
+turtle.right(140)
+turtle.circle(50,200)
+turtle.forward(133)
+turtle.end_fill()
