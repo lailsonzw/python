d639abb43023f4d491c7ee59ea600d8e17867d98
lailsonzw <franciscolailsondealmeida0p9@gmail.com>
2024-01-27 09:36:01 -0300

    primeira vez colocando audio no python

---
+++ exercicioscurso/exercicio020/exercicio020.py
@@ -0,0 +1,6 @@
+#META: faça um progama em python que abra e reproduza o áudio de um arquivo MP3;
+
+import playsound
+
+playsound.playsound('/home/lailson/Desktop/Github/repositories/python/exercicioscurso/exercicio020/audiopy.ogg')
+playsound.playsound('/home/lailson/Desktop/Github/repositories/python/exercicioscurso/exercicio020/motivação.mp3')
--- exercicioscurso/exercico020.py
+++ exercicioscurso/exercico020.py
@@ -1,1 +1,14 @@
 #META: criar um progama que sorteie uma lista sem repitir nomes;
+
+from random import shuffle
+
+name1 = input('Digite o primeiro name:\n')
+name2 = input('Digite o segundo name:\n')
+name3 = input('Digite o terceiro name:\n')
+name4 = input('Digite o quarto nome:\n')
+list = [name1, name2, name3, name4]
+
+shuffle(list)
+
+print('A ordem de apresentação será\n')
+print(list)
