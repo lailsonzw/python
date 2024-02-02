2981232aeac54c064e44b1b46c7967f00ce2a123
lailsonzw <franciscolailsondealmeida0p9@gmail.com>
2024-02-02 10:10:13 -0300

    novas atualizacoes

---
+++ mundo1/coresterminal/comomudaracordoterminal.py
@@ -0,0 +1,12 @@
+#META: aprender a mudar a cor do terminal;
+
+print('\033[0;30;40mHello, wold\n')
+print('\033[1;31;41mHello, world\n')
+print('\033[4;32;42mHello, world\n')
+print('\033[7;33;43mHello, world\n')
+print('\033[0;34;44mHello, world\n')
+print('\033[1;35;45mHello, world\n')
+print('\033[4;36;46mHello, world\n')
+print('\033[7;37;47mHello, world\n')
+print('\033[1;31;0mHello, world\n')
+print('\033[1;0;41fmHello, world\n')
--- mundo1/exercicios/pagamento.py
+++ mundo1/exercicios/pagamento.py
@@ -18,8 +18,11 @@ else:
     else:
         precofinal = produto - desconto
 
-print('o preço final ficou: {}'.format(precofinal))
-confirmar = input('deseja confirmar?\n1-sim ou 2-não\nDigite aqui:')
+print('o preço final ficou: \033[1;33;40m{}\n'.format(precofinal))
+confirmar = int(input('deseja confirmar?\n1-sim ou 2-não\nDigite aqui:'))
+if confirmar == 2:
+    print('compra cancelada.')
+
 
 #novo comando aprendido os.system('comandos relacionados ao seu terminal')
 #esse comando vem da blibioteca "os"
